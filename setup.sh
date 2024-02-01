75a14f97e15908946be4c5f5377f16225178976d
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-02-01 16:42:23 +1100

    add setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+set -x
+
+mkdir coco
+cd coco
+apt-get update -y && apt-get upgrade -y && apt-get install unzip zip -y
+wget -q --show-progress "http://images.cocodataset.org/zips/train2017.zip"
+wget -q --show-progress "http://images.cocodataset.org/zips/val2017.zip"
+wget -q --show-progress "http://images.cocodataset.org/annotations/annotations_trainval2017.zip"
+
+unzip -qq train2017.zip
+unzip -qq val2017.zip
+unzip -qq annotations_trainval2017.zip
