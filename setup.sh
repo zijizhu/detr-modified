a67e13fa5eef82ee48b165c1fce344b33a492bf4
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-02-03 16:14:30 +1100

    update setup

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ apt-get update -y && apt-get upgrade -y && apt-get install unzip zip -y
 wget -q --show-progress "http://images.cocodataset.org/zips/train2017.zip"
 wget -q --show-progress "http://images.cocodataset.org/zips/val2017.zip"
 wget -q --show-progress "http://images.cocodataset.org/annotations/annotations_trainval2017.zip"
+wget -q --show-progress https://dl.fbaipublicfiles.com/detr/detr-r50-e632da11.pth
 
 unzip -qq train2017.zip
 rm train2017.zip
@@ -13,5 +14,6 @@ unzip -qq val2017.zip
 rm val2017.zip
 unzip -qq annotations_trainval2017.zip
 rm annotations_trainval2017.zip
+mv detr-r50-e632da11.pth checkpoints
 
-pip install pycocotools
+pip install scipy pycocotools
