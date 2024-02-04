ba07828f65541d73be3ff9f93db847a105c88af9
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-02-04 21:55:05 +1100

    update relative import

--- models/detr.py
+++ models/detr.py
@@ -6,10 +6,10 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 
-from util import box_ops
-from util.misc import (NestedTensor, nested_tensor_from_tensor_list,
-                       accuracy, get_world_size, interpolate,
-                       is_dist_avail_and_initialized)
+from ..util import box_ops
+from ..util.misc import (NestedTensor, nested_tensor_from_tensor_list,
+                         accuracy, get_world_size, interpolate,
+                         is_dist_avail_and_initialized)
 
 from .backbone import build_backbone
 from .matcher import build_matcher
