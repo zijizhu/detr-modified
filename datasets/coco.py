4246196e07e0309e2e7b2ebac20e978ce99ac2c9
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-02-09 17:49:48 +1100

    extend dataset

--- datasets/coco.py
+++ datasets/coco.py
@@ -149,7 +149,7 @@ def build(image_set, args):
     mode = 'instances'
     PATHS = {
         "train": (root / "train2017", root / "annotations" / f'{mode}_train2017.json'),
-        "val": (root / "val2017", root / "annotations" / f'{mode}_val2017.json')
+        "val": (root / "val2017", root / "annotations" / f'{mode}_val2017.json'),
     }
 
     img_folder, ann_file = PATHS[image_set]
